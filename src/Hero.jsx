02be687168f57099ce0bb3161ac4614112a7fc9c
Benard Haxhijaj <benardhaxhijaj@gmail.com>
2023-12-18 14:55:38 +0100

    Setting up hero

---
+++ src/Hero.jsx
@@ -0,0 +1,25 @@
+const Hero = () => {
+  return (
+    <div className="hero-images">
+      <img className="row-one" src="./src/assets/9.png" alt="Hero Image" />
+      <div className="row-two">
+        <img src="./src/assets/8.png" alt="Hero Image" />
+        <img src="./src/assets/7.png" alt="Hero Image" />
+      </div>
+      <div className="row-three">
+        <img src="./src/assets/1.png" alt="Hero Image" />
+        <img src="./src/assets/2.png" alt="Hero Image" />
+      </div>
+      <div className="row-four">
+        <img src="./src/assets/3.png" alt="Hero Image" />
+        <img src="./src/assets/4.png" alt="Hero Image" />
+      </div>
+      <div className="row-five">
+        <img src="./src/assets/5.png" alt="Hero Image" />
+        <img src="./src/assets/6.png" alt="Hero Image" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav-card">
+      <img src="./src/assets/Logo-svg.svg"></img>
+      <ul className="card-list">
+        <li>About</li>
+        <li>Contact</li>
+        <li>Showcase</li>
+        <li>Shop</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
